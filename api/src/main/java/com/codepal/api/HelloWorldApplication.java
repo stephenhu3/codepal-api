e93db995cb69f0793c11d8db6b2944ef389e43cd
stephenhu3 <stephenhu3@gmail.com>
2016-10-07 19:09:41 -0700

    [Low Risk | Medium Priority] Added REST API skeleton from dropwizard scaffolding

---
+++ api/src/main/java/com/codepal/api/HelloWorldApplication.java
@@ -0,0 +1,103 @@
+package com.codepal.api;
+
+import com.codepal.api.auth.ExampleAuthorizer;
+import com.codepal.api.cli.RenderCommand;
+import com.codepal.api.core.Person;
+import com.codepal.api.core.Template;
+import com.codepal.api.core.User;
+import com.codepal.api.db.PersonDAO;
+import com.codepal.api.filter.DateRequiredFeature;
+import com.codepal.api.health.TemplateHealthCheck;
+import com.codepal.api.tasks.EchoTask;
+import com.codepal.api.auth.ExampleAuthenticator;
+import com.codepal.api.resources.FilteredResource;
+import com.codepal.api.resources.HelloWorldResource;
+import com.codepal.api.resources.PeopleResource;
+import com.codepal.api.resources.PersonResource;
+import com.codepal.api.resources.ProtectedResource;
+import com.codepal.api.resources.ViewResource;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
+import java.util.Map;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    private final HibernateBundle<HelloWorldConfiguration> hibernateBundle =
+        new HibernateBundle<HelloWorldConfiguration>(Person.class) {
+            @Override
+            public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        };
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+
+        bootstrap.addCommand(new RenderCommand());
+        bootstrap.addBundle(new AssetsBundle());
+        bootstrap.addBundle(new MigrationsBundle<HelloWorldConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
+        bootstrap.addBundle(hibernateBundle);
+        bootstrap.addBundle(new ViewBundle<HelloWorldConfiguration>() {
+            @Override
+            public Map<String, Map<String, String>> getViewConfiguration(HelloWorldConfiguration configuration) {
+                return configuration.getViewRendererConfiguration();
+            }
+        });
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        final PersonDAO dao = new PersonDAO(hibernateBundle.getSessionFactory());
+        final Template template = configuration.buildTemplate();
+
+        environment.healthChecks().register("template", new TemplateHealthCheck(template));
+        environment.admin().addTask(new EchoTask());
+        environment.jersey().register(DateRequiredFeature.class);
+        environment.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<User>()
+                .setAuthenticator(new ExampleAuthenticator())
+                .setAuthorizer(new ExampleAuthorizer())
+                .setRealm("SUPER SECRET STUFF")
+                .buildAuthFilter()));
+        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
+        environment.jersey().register(RolesAllowedDynamicFeature.class);
+        environment.jersey().register(new HelloWorldResource(template));
+        environment.jersey().register(new ViewResource());
+        environment.jersey().register(new ProtectedResource());
+        environment.jersey().register(new PeopleResource(dao));
+        environment.jersey().register(new PersonResource(dao));
+        environment.jersey().register(new FilteredResource());
+    }
+}

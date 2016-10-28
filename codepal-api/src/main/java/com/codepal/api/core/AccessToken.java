af04c38cd0670c5d4c268d2d3d8b27529c3574ad
stephenhu3 <stephenhu3@gmail.com>
2016-10-28 14:38:03 -0700

    updated /search to return settings, created endpoints to /accesstokens and /settings to update accessToken and settings

--- codepal-api/src/main/java/com/codepal/api/core/AccessToken.java
+++ codepal-api/src/main/java/com/codepal/api/core/AccessToken.java
@@ -2,22 +2,18 @@ package com.codepal.api.core;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class UserSearch {
+public class AccessToken {
     @JsonProperty(required=true)
     private String accessToken;
 
     @JsonProperty
     private String userId;
 
-    @JsonProperty
-    private String settings;
+    public AccessToken() {}
 
-    public UserSearch() {}
-
-    public UserSearch(String accessToken, String userId, String settings) {
+    public AccessToken(String accessToken, String userId) {
         this.accessToken = accessToken;
         this.userId = userId;
-        this.settings = settings;
     }
 
     public String getAccessToken() {
@@ -34,13 +30,5 @@ public class UserSearch {
 
     public void setUserId(String userId) {
         this.userId = userId;
-    }
-
-    public String getSettings() {
-        return settings;
-    }
-
-    public void setSettings(String settings) {
-        this.settings = settings;
     }
 }
--- codepal-api/src/main/java/com/codepal/api/resources/UserResource.java
+++ codepal-api/src/main/java/com/codepal/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.codepal.api.resources;
 
 import com.codepal.api.MainApplication;
+import com.codepal.api.core.AccessToken;
+import com.codepal.api.core.Settings;
 import com.codepal.api.core.User;
 import com.codepal.api.core.UserSearch;
 import com.datastax.driver.core.*;
@@ -23,7 +25,8 @@ public class UserResource {
     private static Cluster cluster = MainApplication.Cassandra.getCluster();
     private static Session session = MainApplication.Cassandra.getSession();
 
-    public UserResource() {}
+    public UserResource() {
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -39,29 +42,68 @@ public class UserResource {
         LOGGER.warn("accessToken:" + accessToken);
         LOGGER.warn("settings:" + settings);
 
-        cluster = Cluster.builder().addContactPoint("localhost").build();
-        session = cluster.connect("codepal");
-
         PreparedStatement statement = session.prepare(
                 "INSERT INTO users (userId, accessToken, username, settings) VALUES (?,?,?,?);"
         );
         BoundStatement boundStatement = new BoundStatement(statement);
         session.execute(boundStatement.bind(userId, accessToken, username, settings));
-        cluster.close();
     }
 
+    @POST
+    @Path("/accesstokens") // note path is case sensitive
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void updateAccessToken(AccessToken token) {
+        String accessToken = token.getAccessToken();
+        String userId = token.getUserId();
+
+        LOGGER.warn("accessToken:" + accessToken);
+        LOGGER.warn("userId:" + userId);
+
+        PreparedStatement statement = session.prepare(
+                "UPDATE users SET accessToken = :accessToken WHERE userId = :userId;"
+        );
+        BoundStatement boundStatement = new BoundStatement(statement);
+        session.execute(boundStatement.bind()
+                .setString("accessToken", accessToken)
+                .setString("userId", userId));
+    }
+
+    @POST
+    @Path("/settings")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void updateSettings(Settings setting) {
+        String settings = setting.getSettings();
+        String userId = setting.getUserId();
+
+        LOGGER.warn("settings:" + settings);
+        LOGGER.warn("userId:" + userId);
+
+        PreparedStatement statement = session.prepare(
+                "UPDATE users SET settings = :settings WHERE userId = :userId;"
+        );
+        BoundStatement boundStatement = new BoundStatement(statement);
+        session.execute(boundStatement.bind()
+                .setString("settings", settings)
+                .setString("userId", userId));
+    }
+
+    // Return settings and userId
     @POST
     @Path("/search")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public UserSearch getUserId(UserSearch search) {
+    public UserSearch searchUser(AccessToken search) {
         String accessToken = search.getAccessToken();
+
         Statement select = QueryBuilder.select().all()
                 .from("codepal", "users")
                 .where(eq("accessToken", accessToken));
         ResultSet rs = session.execute(select);
         Row row = rs.one();
-        cluster.close();
-        return new UserSearch(row.getString("userId"), row.getString("accessToken"));
+        return new UserSearch(row.getString("userId"),
+                row.getString("accessToken"),
+                row.getString("settings"));
     }
 }

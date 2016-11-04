ecc8f277fd49cb83067af876080099cfc9123768
Justin Lane <justin.art.lane@gmail.com>
2016-11-03 20:39:14 -0700

    init tests for execute and util

---
+++ spec/executeSpec.js
@@ -0,0 +1,32 @@
+﻿describe( 'execute' , function () {
+
+    var executeModule;
+
+    beforeEach(function () {
+        ace.config.set('basePath', 'ace');
+        this.result = fixture.load('fixture.html');
+
+        var codeEditor = new CodeEditor({
+            eleId: 'editor',
+            lang: 'JavaScript',
+            execute: {
+                $outConsole: $('#outConsole'),
+            },
+            editor: {
+                $filename: $('#filename'),
+                $extension: $('#extension'),
+                theme: 'monokai'
+            },
+            ui: {
+                $tabContainer: $('#tabContainer'),
+                $langContainer: $('#lang')
+            }
+        });
+        executeModule = codeEditor.execute;
+    });
+
+
+
+
+
+});
---
+++ spec/utilSpec.js
@@ -0,0 +1,42 @@
+﻿describe( 'util' , function () {
+
+    var utilModule;
+
+    beforeEach(function () {
+        ace.config.set('basePath', 'ace');
+        this.result = fixture.load('fixture.html');
+
+        var codeEditor = new CodeEditor({
+            eleId: 'editor',
+            lang: 'JavaScript',
+            execute: {
+                $outConsole: $('#outConsole'),
+            },
+            editor: {
+                $filename: $('#filename'),
+                $extension: $('#extension'),
+                theme: 'monokai'
+            },
+            ui: {
+                $tabContainer: $('#tabContainer'),
+                $langContainer: $('#lang')
+            }
+        });
+        utilModule = codeEditor.util;
+    });
+
+    //   template 
+
+//  it('', function () {
+//      expect().toBe();
+//  });
+
+    it('checks if param is integer', function () {
+        var integer =  1;
+        expect(utilModule.isInteger(integer).toBe(true));
+        var rational = 0.5;
+        expect(utilModule.isInteger(rational).toBe(false));
+    });
+
+
+});

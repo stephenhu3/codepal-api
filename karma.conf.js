183fb24acf424d999bf4552ee087a963e02e13a5
Justin Lane <justin.art.lane@gmail.com>
2016-11-05 20:30:44 -0700

    updated package.json and karma.conf.js - run npm install to get up to date

--- karma.conf.js
+++ karma.conf.js
@@ -10,8 +10,8 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'fixture'],
-
+    frameworks: ['jasmine-ajax', 'jasmine', 'fixture'],
+ //   plugins: ['karma-jasmine-ajax'],
 
     // list of files / patterns to load in the browser
     files: [

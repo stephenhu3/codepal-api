1ba54fa3ed7cbe8af23a9ce848b650abd371997f
Alec Ng <alecng94@gmail.com>
2016-11-25 20:32:18 -0800

    include new bindings and snippet modules

--- js/codeeditor/main.js
+++ js/codeeditor/main.js
@@ -5,13 +5,16 @@
 function CodeEditor(options) {
 
 	// define Modules and pass in options
-	this.util = this._util();
-	this.editor = this._editor(options.editor);
-	this.execute = this._execute(options.execute);
-	this.ui = this._ui(options.ui);
+	this.util 		= this._util();
+	this.editor 	= this._editor(options.editor);
+	this.execute 	= this._execute(options.execute);
+	this.ui 		= this._ui(options.ui);
+	this.bindings	= this._bindings();
+	this.snippet	= this._snippet();
 
 	// init
 	this.editor.initEditor(options.eleId, options.lang);
 	this.ui.setLang(options.lang);
 	this.ui.setTheme(options.editor.theme);
+	this.execute.initReplClient(options.lang);
 }

1e36fb98ed4f44f7de0e489ac74246cade0ef554
Alec Ng <alecng94@gmail.com>
2016-11-25 21:29:35 -0800

    changed initialization routine

--- js/codeeditor/main.js
+++ js/codeeditor/main.js
@@ -5,16 +5,16 @@
 function CodeEditor(options) {
 
 	// define Modules and pass in options
-	this.util 		= this._util();
-	this.editor 	= this._editor(options.editor);
+	this.util 		= this._util(options.util);
 	this.execute 	= this._execute(options.execute);
 	this.ui 		= this._ui(options.ui);
+	this.editor 	= this._editor();
 	this.bindings	= this._bindings();
 	this.snippet	= this._snippet();
 
 	// init
-	this.editor.initEditor(options.eleId, options.lang);
+	this.editor.initEditor(options.eleId, options.lang, options.theme);
 	this.ui.setLang(options.lang);
-	this.ui.setTheme(options.editor.theme);
+	this.ui.setTheme(options.theme);
 	this.execute.initReplClient(options.lang);
 }
